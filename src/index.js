8d479346d57592f6f429e90d5dcdedad73728bbb
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2018-06-23 17:10:46 +0100

    chores: Unregister ServiceWorker

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 import App from './App';
 // import _GapiService from './services/GapiService';
 // import {gapiParams} from "./config/gapi";
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker'; // -- enable client-side caching
+import { unregister } from './registerServiceWorker'; // -- disable client-side caching
+unregister(); // -- disable client-side caching
 
 // instantiate services
 // const GapiService = new _GapiService(gapiParams);
 
 // ReactDOM.render(<App gapi={GapiService} />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker(); -- enable client-side caching
