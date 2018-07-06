79f688355b9f7830efc8036f47eff0f6665dcf6e
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-07-06 16:14:33 +0300

    Announcement: Put buttons on a dedicated paragraph

--- src/components/Announcement/Announcement.js
+++ src/components/Announcement/Announcement.js
@@ -16,8 +16,8 @@ export default class Announcement extends Component {
   render() {
     return (
       <Alert bsStyle={this.props.announcementStyle} onDismiss={this.handleClose}>
-        <strong>{this.props.title}</strong> {this.props.message}
-        {' '}<Button bsStyle="info" onClick={this.props.action}>{this.props.callToAction}</Button> <Button onClick={this.handleClose}>Dismiss</Button>
+        <p><strong>{this.props.title}</strong> {this.props.message}</p>
+        <p><Button bsStyle="info" onClick={this.props.action}>{this.props.callToAction}</Button> <Button onClick={this.handleClose}>Dismiss</Button></p>
       </Alert>
     );
   }
