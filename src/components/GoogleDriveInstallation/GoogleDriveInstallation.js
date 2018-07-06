5dfee0d4d0b2fbbda1682a5281c30906b534ac6d
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-07-06 17:52:39 +0300

    GoogleDriveInstallation: Improve UX

--- src/components/GoogleDriveInstallation/GoogleDriveInstallation.js
+++ src/components/GoogleDriveInstallation/GoogleDriveInstallation.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import queryString from 'qs';
-import { Button, Carousel, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Button, Carousel, Image, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import ButtonWorksWithGoogleDrive from "../ButtonWorksWithGoogleDrive/ButtonWorksWithGoogleDrive";
 import ButtonInstallOnGoogleDrive from "../ButtonInstallOnGoogleDrive/ButtonInstallOnGoogleDrive";
@@ -43,26 +43,18 @@ export default class GoogleDriveInstallation extends Component {
   render() {
     console.log('GoogleDriveInstallation.render()', this.state);
     return (
-      <div className={styles.container}>
+      <div className={styles.page}>
+        <h2><Image src="./ico/android-icon-48x48.png" /> Merge Google Slides at a breeze!</h2>
         <div>
-          <h3>First time here?</h3>
-          <div>
-            <ButtonInstallOnGoogleDrive bsSize="large" href={this.props.gapi.getGDriveInstallationUrl().toString()} />
-          </div>
-          <div className={styles.carouselContainer}>{this.renderCarousel(images)}</div>
-          <div>
-            <Button target="_blank" href="https://oleksiyrudenko.github.io/merge-google-slides/?state=%7B%22exportIds%22:%5B%221sLBRTLD5fTsRIi5rr-ndVD554G9odLdbHdFDfWY8Y10%22,%221L2NSaN4dPK0u8iuiqDtt5RaWHEeRZvY0Eybc_TPhXTw%22,%221ri6oKxQFjElzFhMPE_NeiQZh7xYY6lOA02Azc_ZJroQ%22%5D,%22action%22:%22open%22,%22userId%22:%22109188810585487526474%22%7D">Try with demo Slides</Button>
-          </div>
-          <div>
-            <ButtonWorksWithGoogleDrive href="https://chrome.google.com/webstore/detail/merge-google-slides/apjijeojdpjochnfenmmkegnifcgfaam" />
-          </div>
+          <ButtonWorksWithGoogleDrive href="https://chrome.google.com/webstore/detail/merge-google-slides/apjijeojdpjochnfenmmkegnifcgfaam" />
         </div>
-        <div>
-          <h3>Already have the app installed?</h3>
-          {this.props.gapi.state.isSignedIn ?
-            <React.Fragment>
-                <div>You've been already signed in.</div>
-                <div>To select Slides to merge navigate to either
+        <hr />
+        <div className={styles.container}>
+          <div>
+            {this.props.gapi.state.isSignedIn
+              ? <React.Fragment>
+                <h3>You've been already signed in.</h3>
+                <div>To select Slides to merge navigate to either of
                   <ListGroup>
                     <ListGroupItem href="https://drive.google.com/drive/" target="_blank">
                       <img src="./assets/Google-Drive-icon-x24.png" alt="Google Drive icon" /> Google Drive™
@@ -74,9 +66,39 @@ export default class GoogleDriveInstallation extends Component {
                     </LinkContainer>
                   </ListGroup>
                 </div>
-            </React.Fragment>
-            : <ButtonSignInWithGoogle />
-            }
+              </React.Fragment>
+              : <React.Fragment>
+                <h3>First time here?</h3>
+                <div>
+                  <ButtonInstallOnGoogleDrive bsSize="large" href={this.props.gapi.getGDriveInstallationUrl().toString()} />
+                </div>
+                <h3>Already have the app installed?</h3>
+                <p>Please, sign in with your Google account</p>
+                <ButtonSignInWithGoogle />
+                <p><b>I did before. Why again?</b></p>
+                <p>Possible reasons are:</p>
+                <ul>
+                  <li>you might have logged out before</li>
+                  <li>you might be using another browser or different computer this time</li>
+                  <li>we might have changed required features from Google API</li>
+                </ul>
+                <p>
+                  The sign in is required for the app to load the Slides to merge. Please, review carefully the powers you grant to this app.
+                  You may also want to make yourself familiar
+                  with <a href="https://oleksiyrudenko.github.io/merge-google-slides--privacy-policy.html">Privacy Policy</a> at this stage.
+                </p>
+                <p><b>Is this app verified by Google?</b></p>
+                <p>It is under revision yet. Not sure yet when and how the verification process would complete.
+                  Please, come back later or when prompted click <u>Advanced</u> and then <u>Go to Merge Google Slides (unsafe)</u>.</p>
+              </React.Fragment>}
+          </div>
+          <div>
+            <h3>How to use?</h3>
+            <div className={styles.carouselContainer}>{this.renderCarousel(images)}</div>
+            <div>
+              <Button target="_blank" href="https://oleksiyrudenko.github.io/merge-google-slides/?state=%7B%22exportIds%22:%5B%221sLBRTLD5fTsRIi5rr-ndVD554G9odLdbHdFDfWY8Y10%22,%221L2NSaN4dPK0u8iuiqDtt5RaWHEeRZvY0Eybc_TPhXTw%22,%221ri6oKxQFjElzFhMPE_NeiQZh7xYY6lOA02Azc_ZJroQ%22%5D,%22action%22:%22open%22,%22userId%22:%22109188810585487526474%22%7D">Try it with demo Slides (read-only mode)</Button>
+            </div>
+          </div>
         </div>
       </div>
     );
