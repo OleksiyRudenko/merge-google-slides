17e20caa378a249b7e412f7ffce7700a380cfc80
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-07-10 13:58:54 +0300

    xobject: Add more utils to manipulate objects

--- src/utils/xobject/xobject.js
+++ src/utils/xobject/xobject.js
@@ -1,6 +1,6 @@
 /**
  * Creates a copy of obj where each path ends in either of props.
- * filterProps({
+ * oFilterProps({
  *   objectId: {any:any},
  *   a: { b0: { c: 'AAA'}, b1: { objectId: 'x', b10: {} } },
  *   c: {}
@@ -11,6 +11,7 @@
  * When either of props is met then entire path is preserved
  * @param {Object} obj
  * @param {Array} props
+ * @returns {Object}
  */
 export const oFilterProps = (obj, props) => {
   let resultingObject = {};
@@ -46,6 +47,119 @@ export const oFilterProps = (obj, props) => {
     }
   });
   // console.log('<<<<<<<<< oFilterProps() returning ', resultingObject);
+  return resultingObject;
+};
+
+/**
+ * Creates an array of values from props.
+ * oCollectValuesFromProps({
+ *   objectId: {any:any},
+ *   a: { b0: { c: 'AAA'}, b1: { objectId: 'x', b10: {} } },
+ *   c: {}
+ * }, 'objectId') ==> [
+ *   {any:any},
+ *   'x',
+ * ]
+ * When either of props is met then its value listed and its children are not traversed
+ * @param {Object} obj
+ * @param {Array} props
+ * @param {Boolean} deeper whether to dig into found prop value if the value is an Object as well
+ * @returns {Array}
+ */
+export const oCollectValuesFromProps = (obj, props, deeper = false) => {
+  let resultingArray = [];
+  // console.log('>>>>>> oCollectValuesFromProps() searching among ', Object.keys(obj), obj);
+  Object.keys(obj).forEach(key => {
+    if (props.includes(key)) {
+      // console.log('oCollectValuesFromProps() found!', key, obj[key]);
+      resultingArray.push(obj[key]);
+      if (deeper) {
+        if (Array.isArray(obj[key])) {
+          // resultingArray = resultingArray.concat(['NESTED ARRAY FOUND ' + JSON.stringify(obj[key])]);
+          const arr = obj[key].map(el => {
+            if (isObject(el)) {
+              const subArray = oCollectValuesFromProps(el, props, deeper);
+              if (subArray.length) {
+                return subArray;
+              } else {
+                return null;
+              }
+            }
+            return null;
+          }).filter(el => !!el);
+          // console.log('oCollectValuesFromProps() array container', arr);
+          if (arr.length) {
+            resultingArray = resultingArray.concat(...arr);
+          }
+        } else {
+          if (isObject(obj[key])) {
+            // resultingArray = resultingArray.concat(['NESTED OBJECT FOUND ' + JSON.stringify(obj[key])]);
+            // traverse deeper
+            const subArray = oCollectValuesFromProps(obj[key], props, deeper);
+            if (subArray.length) {
+              resultingArray = resultingArray.concat(subArray);
+            }
+          }
+        }
+      }
+    } else {
+      if (isObject(obj[key])) {
+        // traverse deeper
+        const subArray = oCollectValuesFromProps(obj[key], props, deeper);
+        if (subArray.length) {
+          resultingArray = resultingArray.concat(subArray);
+        }
+      } else {
+        if (Array.isArray(obj[key])) {
+          const arr = obj[key].map(el => {
+            if (isObject(el)) {
+              const subArray = oCollectValuesFromProps(el, props, deeper);
+              if (subArray.length) {
+                return subArray;
+              } else {
+                return null;
+              }
+            }
+            return null;
+          }).filter(el => !!el);
+          // console.log('oCollectValuesFromProps() array container', arr);
+          if (arr.length) {
+            resultingArray = resultingArray.concat(...arr);
+          }
+        }
+      }
+    }
+  });
+  // console.log('<<<<<<<<< oCollectValuesFromProps() returning ', resultingArray);
+  return resultingArray;
+};
+
+/**
+ * Returns array of strings and numbers only
+ * @param {Array} arr
+ * @param {Boolean} noNumbers - no typeof numbers
+ * @returns {Array}
+ */
+export const aFilterAlphaNumerics = (arr, noNumbers = false) => arr.filter(el => {
+  let t = typeof el;
+  return (t === 'string' || (t === 'number' && !noNumbers));
+});
+
+/**
+ * Converts an array into object where keys are top-level values from array and values are undefined.
+ * Non-number|string elements are JSON.stringified
+ * @param {Array} arr
+ * @param {Boolean} filterAlphaNumerics remove anything else but numbers or strings from arr
+ * @returns {Object}
+ */
+export const arrayToObjectKeys = (arr, filterAlphaNumerics = true) => {
+  let resultingObject = {};
+  if (filterAlphaNumerics) { arr = aFilterAlphaNumerics(arr); }
+  arr
+    .map(el => typeof el === 'object' ? JSON.stringify(el) : el)
+    .forEach(el => {
+      resultingObject[el] = undefined;
+  });
   return resultingObject;
 };
 
--- src/utils/xobject/xobject.test.js
+++ src/utils/xobject/xobject.test.js
@@ -32,8 +32,187 @@ it('filters object by props', () => {
     },
   };
 
-  console.log(filtered);
-  console.log(expected);
-
   expect(filtered).toEqual(expected);
+});
+
+it('collects props\' values from object', () => {
+  const collected = xobject.oCollectValuesFromProps({
+    objectId: {any: 'any'},
+    a: {
+      objectId: 'x0',
+      b0: {
+        c: 'AAA'
+      },
+      b1: {
+        objectId: 'x1',
+        b10: {}
+      },
+    },
+    c: {},
+    d: {
+      e: 'EEEE',
+      f: [],
+      g: [ 'aaaa', 'bbbb', {h: { objectId: 'x2', i: 'IIII' } }   ],
+    },
+    j: {
+      objectId: {
+        k: 'KKK',
+        objectId: 'x3',
+      },
+      m: 'MMMM',
+    }
+  }, ['objectId']);
+
+  const expected = [
+    {any: 'any'},
+    'x0',
+    'x1',
+    'x2',
+    { k: 'KKK', objectId: 'x3', },
+  ];
+
+  expect(collected).toEqual(expected);
+});
+
+it('collects props\' values from object even deeper', () => {
+  const collected = xobject.oCollectValuesFromProps({
+    objectId: {any: 'any'},
+    a: {
+      objectId: 'x0',
+      b0: {
+        c: 'AAA'
+      },
+      b1: {
+        objectId: 'x1',
+        b10: {}
+      },
+    },
+    c: {},
+    d: {
+      e: 'EEEE',
+      f: [],
+      g: [ 'aaaa', 'bbbb', {h: { objectId: 'x2', i: 'IIII' } }   ],
+    },
+    j: {
+      objectId: {
+        k: 'KKK',
+        objectId: 'x3',
+      },
+      m: 'MMMM',
+    },
+    n: {
+      objectId: ['x4', { objectId: 'x5'} ]
+    }
+  }, ['objectId'], true);
+
+  const expected = [
+    {any: 'any'},
+    'x0',
+    'x1',
+    'x2',
+    { k: 'KKK', objectId: 'x3', },
+    'x3',
+    ['x4', { objectId: 'x5'} ],
+    'x5',
+  ];
+
+  expect(collected).toEqual(expected);
+});
+
+it('filters alphanumerics from array', () => {
+  const collected = xobject.aFilterAlphaNumerics([
+    'abc',
+    '5',
+    6,
+    '7a',
+    'a8',
+  ]);
+
+  const expected = [
+    'abc',
+    '5',
+    6,
+    '7a',
+    'a8',
+  ];
+
+  expect(collected).toEqual(expected);
+});
+
+it('filters alphanumerics from array strictly', () => {
+  const collected = xobject.aFilterAlphaNumerics([
+    'abc',
+    '5',
+    6,
+    '7a',
+    'a8',
+  ], true);
+
+  const expected = [
+    'abc',
+    '5',
+    '7a',
+    'a8',
+  ];
+
+  expect(collected).toEqual(expected);
+});
+
+it('converts an array into object with keys from array elements excluding non-string|number values', () => {
+  const factual = xobject.arrayToObjectKeys([
+    'abc',
+    '5',
+    {abc: 'def'},
+    6,
+    ['j', 'k'],
+    '7a',
+    undefined,
+    'a8',
+    null,
+    5,
+    6,
+  ]);
+
+  const expected = {
+    'abc': undefined,
+    '5': undefined,
+    6: undefined,
+    '7a': undefined,
+    'a8': undefined,
+  };
+
+  console.log(factual);
+
+  expect(factual).toEqual(expected);
+});
+
+it('converts an array into object with keys from array elements thorough', () => {
+  const factual = xobject.arrayToObjectKeys([
+    'abc',
+    '5',
+    {abc: 'def'},
+    6,
+    ['j', 'k'],
+    null,
+    '7a',
+    undefined,
+    'a8',
+    null,
+  ], false);
+
+  const expected = {
+    'abc': undefined,
+    '5': undefined,
+    "{abc: 'def'}": undefined,
+    6: undefined,
+    "['j', 'k']": undefined,
+    '7a': undefined,
+    'undefined': undefined,
+    'a8': undefined,
+    'null': undefined,
+  };
+
+  console.log(factual);
+
+  expect(factual).toEqual(expected);
 });
