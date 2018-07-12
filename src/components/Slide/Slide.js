2207d12dca03e8b62a137e4692c420d73c707a1f
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-07-12 19:00:10 +0300

    Slide: Employ Component.getDerivedStateFromProps() to manage async requests

--- src/components/Slide/Slide.js
+++ src/components/Slide/Slide.js
@@ -2,45 +2,65 @@ import React, { Component } from 'react';
 import { ProgressBar } from 'react-bootstrap';
 import styles from "./Slide.css";
 import SourceDecksService from "../../services/SourceDecksService";
-import {bindHandlers} from "../../utils/bind";
 
 export default class Slide extends Component {
+  static defaultProps = {
+    deckId: null,
+    slideId: null,
+  };
+
   constructor(props) {
     super(props);
     this.debug = true;
     this.state = {
       deckId: this.props.deckId,
       slideId: this.props.slideId,
-      slideThumbnailUrl: undefined,
+      slideThumbnailUrl: null,
       isError: false,
-      errorMessage: undefined,
+      errorMessage: null,
     };
-    bindHandlers(this, 'loadThumbnail');
   }
 
   get fullSlideId() { return this.props.deckId + '.' + this.props.slideId; }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    // Store changing props data in state to compare when props change
+    // Clear out any previously-loaded user data not to render stale stuff.
+    if (nextProps.deckId !== prevState.deckId) {
+      return {
+        deckId: nextProps.deckId,     // this is also a flag for whether to load any data
+        slideId: nextProps.slideId,
+        slideThumbnailUrl: null, // this is a flag to load data
+        isError: false,
+        errorMessage: null,
+      };
+    }
+    // otherwise state update on external update signal not necessary
+    return null;
+  }
 
   /**
    * Update state on first mount
    */
   componentDidMount() {
     this.debug && console.log('Slide.cDM()');
-    this.loadThumbnail();
+    // load external data first time upon mounting
+    this._loadThumbnail();
   }
 
   /**
    * Update state snapshot and derived state properties based on props changed
    */
-  componentDidUpdate() {
-    this.debug && console.log('Slide.cDU()');
-    if (this.props.slideId === undefined || this.props.deckId !== this.state.deckId || this.props.slideId !== this.state.slideId) {
-      this.setState({
-        deckId: this.props.deckId,
-        slideId: this.props.slideId,
-        slideThumbnailUrl: undefined,
-        isError: false,
-      }, this.loadThumbnail());
+  componentDidUpdate(prevProps, prevState) {
+    this.debug && console.log('Slide.cDU(), prevProps, props, prevState, state', prevProps, this.props, prevState, this.state);
+    if (this.state.slideThumbnailUrl === null && this.state.deckId && this.state.slideId) {
+      // we're in commit phase, safe to load new data provided conditions are met
+      this._loadThumbnail();
     }
+  }
+
+  componentWillUnmount() {
+    this.debug && console.error('Slide.cWU() CANCEL PENDING REQUEST!', this.props, this.state);
   }
 
   /**
@@ -69,20 +89,23 @@ export default class Slide extends Component {
   /**
    * Load/update thumbnail
    */
-  loadThumbnail() {
-    this.debug && console.log('Slide.loadThumbnail()', this.props);
+  _loadThumbnail() {
+    this.debug && console.log('Slide._loadThumbnail()', this.props);
     if (!this.props.deckId || !this.props.slideId) {
       return;
     }
+    // cancel any pending requests for the same resource from the same unique source!
+    // const requestId = componentInstance + deckId + slideId
+
     SourceDecksService.getThumbnail(this.props.deckId, this.props.slideId)
       .then(imageUrl => this.setState({
         slideThumbnailUrl: imageUrl,
         isError: false,
       }))
       .catch(rejection => {
-        console.error('Slide.loadThumbnail() catch', rejection);
+        console.error('Slide._loadThumbnail() catch', rejection);
         this.setState({
-          slideThumbnailUrl: undefined,
+          slideThumbnailUrl: null,
           isError: true,
           errorMessage: {
             short: 'Error. Hover for details',
