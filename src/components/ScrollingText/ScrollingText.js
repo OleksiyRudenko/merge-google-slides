1b1dc1a1e482bbb0599cb8a9313bfe77e5c6015d
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-07-12 10:46:51 +0300

    ScrollingText: Reset isScrolling on props update; cancel timeOut on unmount

--- src/components/ScrollingText/ScrollingText.js
+++ src/components/ScrollingText/ScrollingText.js
@@ -3,34 +3,58 @@ import { ProgressBar } from 'react-bootstrap';
 import styles from "./ScrollingText.css";
 
 export default class ScrollingText extends Component {
+  static defaultProps = {
+    idBase: undefined,
+    textData: undefined,
+  };
+
   constructor(props) {
     super(props);
-    this.debug = true;
+    this.debug = false;
     this.state = {
       isScrolling: true,
     };
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.textData !== prevState.prevTextData) {
+      return {
+        isScrolling: true,
+        prevTextData: nextProps.textData,
+      };
+    }
+    return null;
   }
 
   componentDidUpdate(prevProps, prevState) {
     this.debug && console.log('ScrollingText.cDU() props,state,prevProps,prevState', this.props, this.state, prevProps, prevState);
     if (this.props.textData) {
       // remove scrolling class if <p> width is lesser than <div> width
-      setTimeout(() => {
+      this.timeoutId = setTimeout(function () {
         const stc = document.getElementById('stc' + this.props.idBase);
         const stt = document.getElementById('stt' + this.props.idBase);
         if (stc && stt) {
           const stcw = stc.clientWidth;
           const sttw = stt.offsetWidth;
+          this.debug && console.log('ScrollingText.cDU() setTimeout() elements are in DOM', this.props.textData, stcw, sttw);
           if (stcw > sttw) {
+            this.debug && console.log('ScrollingText.cDU() setTimeout() change state', this.state, this.props.textData, stcw, sttw);
             this.setState({
               isScrolling: false,
             });
           }
-          this.debug && console.log('ScrollingText.cDU()', this.props.textData, stcw, sttw);
         } else {
           console.error('ScrollingText.cDU() -- targeted elements do not exist anymore!', stc, stt);
         }
-      }, 300);
+        this.timeoutId = null;
+      }.bind(this), 300);
+    }
+  }
+
+  componentWillUnmount() {
+    // cancel timeOut
+    if (this.timeoutId) {
+      window.clearTimeout(this.timeoutId);
     }
   }
 
