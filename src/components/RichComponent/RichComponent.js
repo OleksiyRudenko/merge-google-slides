59ecb829dd6227142afc2341f92f85a9ba12216c
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-07-13 13:05:57 +0300

    RichComponent: Add RichComponent to extend React.Component with _debug() method

---
+++ src/components/RichComponent/RichComponent.js
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+import * as xobject from '../../utils/xobject/xobject.js';
+
+export default class RichComponent extends Component {
+  /**
+   * Logs debug message and optional parameters.
+   * @param message
+   * @param args
+   * @private
+   */
+  _debug(message, ...args) {
+    if (this.debug) {
+      args = args.map(item => {
+        return item !== null && typeof item === 'object'
+          ? xobject.deepClone(item)
+          : item;
+      });
+      console.log(this.constructor.name + ' ' + message + ' this.props, this.state:', Object.assign({}, this.props), Object.assign({}, this.state), ...args );
+    }
+  }
+}
