a9532cafbd2c73b29a89774602560c9a5582a580
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-07-19 20:26:51 +0300

    PresentationPage: Add PresentationPage class

---
+++ src/services/GSlidesService/PresentationPage.js
@@ -0,0 +1,55 @@
+import * as xobject from "../../utils/xobject/xobject";
+
+export default class PresentationPage {
+  p = null;
+
+  constructor (page = null, clone = false) {
+    if (page && clone) {
+      this.pageClone = page;
+    } else {
+      this.page = page;
+    }
+  }
+
+  get page() { return this.p; }
+  get pageClone() { return xobject.deepClone(this.p); }
+  set page(page) { this.p = page; }
+  set pageClone(page) { this.p = xobject.deepClone(page); }
+
+  get pageType() {
+    switch (this.p.pageType) {
+      case 'MASTER': return 'Master';
+      case 'NOTES_MASTER': return 'NotesMaster';
+      case 'LAYOUT': return 'Layout';
+      default: return 'Slide';
+    }
+  }
+
+  /**
+   * Creates requests from a page to use with gapi.slides.presentations.batchUpdate
+   * @returns {Array} requests for window.gapi.client.slides.presentations.batchUpdate
+   */
+  createBatchUpdateRequest() {
+    // initialize with create slide request;
+    let requests = [
+      {
+        createSlide: { // https://developers.google.com/slides/reference/rest/v1/presentations/request#CreateSlideRequest
+          objectId: slideId,
+          slideLayoutReference: {
+            predefinedLayout: predefinedLayoutName,
+          },
+          // placeholderIdMappings: [], // https://developers.google.com/slides/reference/rest/v1/presentations/request#LayoutPlaceholderIdMapping
+        }
+      },
+      {
+        updatePageProperties: { // https://developers.google.com/slides/reference/rest/v1/presentations/request#UpdatePagePropertiesRequest
+          objectId: slideId,
+          pageProperties: slide.pageProperties,
+          fields: Object.keys(slide.pageProperties).join(','),
+        }
+      }
+    ];
+
+    return requests;
+  }
+}
